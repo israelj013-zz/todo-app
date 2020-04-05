d2cc73232acb5d50e1f8eb4038ee2bd23b62fede
israeljimenez <israelj@uci.edu>
2020-04-04 17:06:30 -0700

    Updates

---
+++ precache-manifest.12cfa47e219bb5978e2586a99a51b464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417595b936ce8c1a25fd6f23ccf67ab3",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "47da51daa5875278cfba",
+    "url": "/todo-app/static/css/main.0be437e3.chunk.css"
+  },
+  {
+    "revision": "134c3431c4043a023cdb",
+    "url": "/todo-app/static/js/2.fa52947a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.fa52947a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47da51daa5875278cfba",
+    "url": "/todo-app/static/js/main.6b1a7eb7.chunk.js"
+  },
+  {
+    "revision": "7240911c12f8045ea0a0",
+    "url": "/todo-app/static/js/runtime-main.6fd27bfe.js"
+  }
+]);
